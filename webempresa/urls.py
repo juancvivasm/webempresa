24616f227e2d0aae425f7ce6111ec94e555ebc5c
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-11 17:31:52 -0400

    Organizando Mejor nuestras URLS

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -17,17 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 # Esto va a importar de nuestro fichero core las vistas basicas
-from core import views as core_views
+from django.urls import include, path
 
 urlpatterns = [
     # Paths del core
-    path('', core_views.home, name='home'),
-    path('about/', core_views.about, name='about'),
-    path('services/', core_views.services, name='services'),
-    path('store/', core_views.store, name='store'),
-    path('contact/', core_views.contact, name='contact'),
-    path('blog/', core_views.blog, name='blog'),
-    path('sample/', core_views.sample, name='sample'),
+    path('', include('core.urls')),
     # Paths del admin
     path('admin/', admin.site.urls),
 ]
