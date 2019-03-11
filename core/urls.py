f1504ec8ff3bc48c1dda2ab2bb6f902d44f55211
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-11 19:57:10 -0400

    Fusion del FrontEnd con el BackEnd

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+# Importo las vistas que estan en este directorio
 from . import views
 
 urlpatterns = [
--- core/views.py
+++ core/views.py
@@ -2,22 +2,28 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at Inicio home/")
+    #return HttpResponse("Hello, world. You're at Inicio home/")
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse("Hello, world. You're at Historia about/")
+    #return HttpResponse("Hello, world. You're at Historia about/")
+    return render(request, "core/about.html")
 
 def services(request):
-    return HttpResponse("Hello, world. You're at Servicios services/")
+    #return HttpResponse("Hello, world. You're at Servicios services/")
+    return render(request, "core/services.html")
 
 def store(request):
-    return HttpResponse("Hello, world. You're at Visítanos store/")
+    #return HttpResponse("Hello, world. You're at Visítanos store/")
+    return render(request, "core/store.html")
 
 def contact(request):
-    return HttpResponse("Hello, world. You're at Contacto contact/")
+    #return HttpResponse("Hello, world. You're at Contacto contact/")
+    return render(request, "core/contact.html")
 
 def blog(request):
-    return HttpResponse("Hello, world. You're at Blog blog/")
+    #return HttpResponse("Hello, world. You're at Blog blog/")
+    return render(request, "core/blog.html")
 
 def sample(request):
     return HttpResponse("Hello, world. You're at Sample sample/ (esta es para páginas de prueba)")
