ab0114f697f37c5cdbcd81986bc00f0aba2e5ba6
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-12 18:28:43 -0400

    Migracion del modelo service

--- services/models.py
+++ services/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=200, verbose_name = "Título")
+    subtitle = models.CharField(max_length=200, verbose_name = "Subtítulo ")
+    content = models.TextField()
+    image = models.ImageField(verbose_name = "Imagen", upload_to="services" )
+    created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name = "Fecha de Edición")
