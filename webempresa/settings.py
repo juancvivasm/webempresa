3d1a6cee5c29902d980c5e507c738f1593a22d47
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-12 18:07:06 -0400

    Migracion de Django con PostgreSql

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'webempresa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webempresa',
+        'USER': 'postgres',
+        'PASSWORD': 'jc159',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
