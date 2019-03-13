b25d2c6ccef86cf3ff8e095ad70eb4f0f5fcabeb
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-13 14:37:20 -0400

    Se registra la App Blog para utilizar Administrador

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# Crear administrador para nuestro Blog
+from .models import Category, Post
+
+# Se extiende la configuracion del modelo para su administracion
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
