477e4f8546e325f4f5546281f068b394fd52f684
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-14 09:25:29 -0400

    Finalizando la App Blog con sus vistas

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Se agrega el modelo
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 def blog(request):
     # Con el ORM de Django se obtienen los datos en objetos
     posts = Post.objects.all()
-
     return render(request, "blog/blog.html", {'posts':posts})
+
+
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    # posts = Post.objects.filter(categories=category)
+    # return render(request, "blog/category.html", {'category':category, 'posts':posts})
+    return render(request, "blog/category.html", {'category':category})
